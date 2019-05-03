99c5a81ce4b00c7689ff765b07383421c7fc26ec
Orest Dubay <orest3.dubay@gmail.com>
2019-05-04 00:10:31 +0200

    Optimization works

--- test4.py
+++ test4.py
@@ -1,30 +1,13 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-device = torch.device("cuda")
-
-#def uMv(u,M,v):
-#    print (u)
-#    print (u.matmul(M))
-#    print (u.matmul(M).matmul(v.transpose(0,1)))
-#def uMv(u,M,v):
-#    print (u)
-#    print (u.matmul(M))
-#    print ("uM",u.matmul(M).view(-1,1,3))
-#    print ("v",v.view(-1,3,1))
-#    print (u.matmul(M).view(-1,1,3).bmm(v.view(-1,3,1)))
+device = torch.device("cpu")
+scalartype = torch.float32
 
 def uMv(u,M,v):
-#    print ("u",u)
-#    print ("uM",u.matmul(M).view(-1,1,4))
     return (u.matmul(M).view(-1,1,4).bmm(v.view(-1,4,1)))
-
-#uMv(
-#    torch.tensor([[1,0,0],[0,10,0],[1,0,0],[0,10,0],[1,0,0],[0,10,0]]),
-#    torch.tensor([[1,2,3],[4,5,6],[7,8,9]]),
-#    torch.tensor([[11,12,13],[14,15,16],[1,0,0],[0,10,0],[1,0,0],[0,10,0]])
-#    )   
 
 def advance_to(M, positions, vectors):
     pMp = uMv(positions, M, positions)
@@ -39,26 +22,15 @@ def advance_to(M, positions, vectors):
     t1 = (-B + torch.sqrt(D))/(2*vMv)
     t2 = (-B - torch.sqrt(D))/(2*vMv)
 
-#    t=torch.where(vMv==0.0, t0, torch.where(t1<0, t2, torch.where(t2<0,t1,torch.min(t1,t2))))
-    t=t0
-#    print ("positions",positions.size())
-#    print ("vectors",vectors.size())
-#    print ("pMp",pMp.size())
-#    print ("vMp",vMp.size())
-#    print ("pMv",pMv.size())
-#    print ("vMv",vMv.size())
-#    print ("t",t.size())
-#    print (t.view(1,-1).transpose(0,1)*vectors)
+    t=t0                                 #TODO: where did not work with sqrt and autograd
     positions = positions + t.view(1,-1).transpose(0,1)*vectors
     return positions, vectors
 
 def normals_on(M, positions, vectors):
-    P = torch.eye(4,4,device=device)
+    P = torch.eye(4,4,device=device,dtype=scalartype)
     P[3,3]=0
     normals = positions.matmul(M) + M.matmul(positions.transpose(0,1)).transpose(0,1)
-    print("normals",normals)
     normals = normals.matmul(P)
-    print("normals",normals)
 
     nb = normals.view(-1,1,4)
     norm = torch.sqrt(torch.bmm(nb,nb.transpose(1,2)))
@@ -95,42 +67,60 @@ def advance_to_and_reflect(M, positions, vectors, plot=False):
     positions, vectors = reflect(M, positions, vectors, plot=plot)
     return positions, vectors
 
-span = [(x,y) for x in np.linspace(-0.9,0.9,20) for y in np.linspace(-0.9,0.9,20)]
-#span = [(x,y) for x in np.linspace(-0.9,0.9,3) for y in np.linspace(-0.1,0.1,1)]
-positions = torch.tensor([[x,y,4,1] for x,y in span],dtype=torch.float, device=device)
-vectors = torch.tensor([[0,0,-0.5,0] for x,y in span],dtype=torch.float, device=device)
 
 mirror=torch.tensor(    [
-      [ 0.2, 0  , 0  , 0  ],
-      [ 0  , 0.2, 0  , 0  ],
+      [ 0.01, 0.01  , 0  , 0  ],
+      [ 0.01  , 0.05, 0  , 0  ],
       [ 0  , 0  , 0  ,-0.5],
       [ 0  , 0  ,-0.5, 0  ]
-    ], requires_grad=True, dtype=torch.float, device=device)
+    ], requires_grad=True, dtype=scalartype, device=device)
 plane=torch.tensor(    [
       [ 0  , 0  , 0  , 0  ],
       [ 0  , 0  , 0  , 0  ],
       [ 0  , 0  , 0  ,-1  ],
       [ 0  , 0  ,-1  , 2  ]
-    ], requires_grad=False, dtype=torch.float, device=device)
+    ], requires_grad=False, dtype=scalartype, device=device)
 center = torch.tensor(
       [ 0  , 0  , 1  , 1  ],
-    requires_grad=False, dtype=torch.float, device=device)
+    requires_grad=False, dtype=scalartype, device=device)
 
+def rays():
+    span = [(x,y) for x in np.linspace(-0.9,0.9,200) for y in np.linspace(-0.9,0.9,200)]
+    #span = [(x,y) for x in np.linspace(-0.9,0.9,3) for y in np.linspace(-0.1,0.1,1)]
+    positions = torch.tensor([[x,y,4,1] for x,y in span],dtype=scalartype, device=device)
+    vectors = torch.tensor([[0,0,-0.5,0] for x,y in span],dtype=scalartype, device=device)
+    return positions, vectors
 
+positions, vectors = rays()
 plot_rays(positions, vectors, color="r")
-positions, vectors = advance_to_and_reflect(mirror, positions, vectors)
-plot_rays(positions, vectors, color="g")
-positions, vectors = advance_to(plane,positions, vectors)
 
-d = positions-center
-dist = (d*d).sum()
-print(d)
-print(dist)
-dist.backward()
-print (mirror.grad)
+start = time.time()
+for i in range(100):
+    positions, vectors = rays()
+    positions, vectors = advance_to_and_reflect(mirror, positions, vectors)
+    positions, vectors = advance_to(plane,positions, vectors)
 
-plot_rays(positions,vectors,color="b")
-plt.plot([0],[1],"k+")
-plt.xlim(-2,2)
-plt.ylim(-1,6)
-plt.show()
+    d = positions-center
+    dist = (d*d).sum()
+    print(i,dist)
+    dist.backward()
+
+    rate = 0.0000015
+    g=mirror.grad.cpu().numpy()
+    mirror = mirror.detach().cpu()
+    m = mirror.numpy()
+    g[3,:]=0
+    g[:,3]=0
+    g[2,:]=0
+    g[:,2]=0
+    m-=g*rate
+    mirror = torch.tensor(m, requires_grad=True, dtype=scalartype, device=device)
+end = time.time()
+
+print(mirror)
+print(end-start)
+#plot_rays(positions, vectors, color="g")
+#plt.plot([0],[1],"k+")
+#plt.xlim(-2,2)
+#plt.ylim(-1,6)
+#plt.show()
